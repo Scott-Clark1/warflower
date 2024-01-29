59d5448bfc3bc6eb8f4f6a9f23c9bba44515e4a3
Scott Clark <scott@scottclark.ai>
2024-01-29 02:08:49 +0000

    barebones functionality!

--- warflower/controller.py
+++ warflower/controller.py
@@ -1,27 +1,39 @@
+import logging
 import os
-import yaml
 import sys
+import yaml
 
 from warflower.docker import DockerManager
 from warflower.util import to_serverid
 
+logger = logging.getLogger("webapp")
+
 class ServerManager:
   def __init__(self):
-    currdir = os.path.dirname(__file__)
-    gamesdir = os.path.join(currdir, "games")
-    games = os.listdir(gamesdir)
-
     self.all_servers = {}
-    for game in games:
-      gamecfgs = yaml.safe_load(open(os.path.join(gamesdir, game)))
-      gamename = os.path.splitext(os.path.basename(game))[0]
-      for cfg in gamecfgs:
-        self.all_servers[to_serverid(gamename, cfg)] = gamecfgs[cfg]
+    self.load_configs()
 
     self.docker_mgmt = DockerManager()
     self.active_servers = {}
 
+  def load_configs(self):
+    currdir = os.path.dirname(__file__)
+
+    all_servers = {}
+    gamesdir = os.path.join(currdir, "games")
+    games = os.listdir(gamesdir)
+    for game in games:
+      if game[-5:] != ".yaml":
+        continue
+      gamecfgs = yaml.safe_load(open(os.path.join(gamesdir, game)))
+      gamename = os.path.splitext(os.path.basename(game))[0]
+      for cfg in gamecfgs:
+        all_servers[to_serverid(gamename, cfg)] = gamecfgs[cfg]
+    self.all_servers = all_servers
+
   def list_configs(self):
+    self.load_configs()
+
     servers = self.all_servers
     res = {}
     for serv in servers:
@@ -33,27 +45,28 @@ class ServerManager:
 
 
   def start_server(self, serverid, rt_args={}):
-    print("TRYING TO START: " + serverid, file=sys.stdout)
+    self.load_configs()
+
+    logger.info(f"Launching {serverid} with these rtargs:")
     cfg = self.all_servers[serverid]
 
-    rt_args = {**cfg["runtime_args"], **rt_args}
+    rt_args = {**cfg["runtime_args"]} # , **rt_args}
     self.active_servers[serverid] = {}
 
-    print("OK, TRYING THIS SHIT OUT!", file=sys.stdout)
-    print(cfg["image"], cfg["command"], serverid, file=sys.stdout)
-    self.docker_mgmt.start(cfg["image"], cfg["command"], serverid) # , **rt_args)
+    logger.info("OK, TRYING THIS SHIT OUT!")
+    logger.info(f"{cfg['image']}, {cfg['command']}, {serverid}")
+    logger.error(f"{rt_args}")
+    self.docker_mgmt.start(cfg["image"], cfg["command"], serverid, **rt_args)
     return True
 
   def stop_server(self, serverid=None):
+    logger.info("Shutting down " + serverid)
+    self.load_configs()
+
     self.docker_mgmt.stop(serverid)
     self.active_servers.pop(serverid, None)
     return True
 
-    
-
-
 
 if __name__ == "__main__":
   sm = ServerManager()
-  import pdb; pdb.set_trace()
-  pass
--- warflower/webapp/main.py
+++ warflower/webapp/main.py
@@ -3,17 +3,28 @@ from flask_httpauth import HTTPTokenAuth
 import os
 import yaml
 
+import logging
+
 from warflower.controller import ServerManager
 
 
-mgmt = ServerManager()
+app = Flask("webapp")
 
-app = Flask(__name__)
+file_handler = logging.FileHandler('/home/ubuntu/logs/flask.log', encoding='utf-8', mode='w+')
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+app.logger.info("Starting up!")
+app.logger.warning("Starting up warning!")
+
 auth = HTTPTokenAuth(scheme='Bearer')
 
 secrets = yaml.safe_load(open(os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "tokens.secret.yaml"
   )))
+
+
+MGMT = ServerManager()
 
 @auth.verify_token
 def verify_token(token):
@@ -29,13 +40,13 @@ def hello_world():
 @app.route('/list')
 @auth.login_required
 def list_configs():
-  return jsonify({"status" : 200, "data" : mgmt.list_configs()})
+  return jsonify({"status" : 200, "data" : MGMT.list_configs()})
 
 
 @app.route('/start/<serverid>', methods=['POST'])
 @auth.login_required
 def start_server(serverid):
-  res = mgmt.start_server(serverid)
+  res = MGMT.start_server(serverid)
   if res:
     return jsonify({"status" : 200, "ok" : True})
   return jsonify({"status" : 500, "ok" : False})
@@ -43,7 +54,7 @@ def start_server(serverid):
 @app.route('/stop/<serverid>', methods=['POST'])
 @auth.login_required
 def stop_server(serverid):
-  res = mgmt.stop_server(serverid)
+  res = MGMT.stop_server(serverid)
   if res:
     return jsonify({"status" : 200, "ok" : True})
   return jsonify({"status" : 500, "ok" : False})
@@ -51,4 +62,4 @@ def stop_server(serverid):
 
 if __name__ == '__main__':
   # starting app
-  app.run(debug=True,host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0')
